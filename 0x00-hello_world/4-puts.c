8d857a1b4b4c51593d265d8fc629720e3be4ec99
joshuayamoahyankson <ziriks360@gmail.com>
2023-02-22 03:41:34 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print "Programming is like building a multilingual ple"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
