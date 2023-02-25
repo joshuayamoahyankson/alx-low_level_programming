e7d26a3975158ea181ab17d8e82b89974b8ddfca
joshuayamoahyankson <ziriks360@gmail.com>
2023-02-25 06:29:46 +0000

    2-mul.c, Write a function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 values
+ * @a: first value.
+ * @b: second value.
+ * Description: Write a function that multiplies two integers.
+ * Return: The product (int).
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
+
