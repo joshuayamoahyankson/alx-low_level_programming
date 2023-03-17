cbd91a68f3d251357968f15dd23652420b7bb039
joshuayamoahyankson <ziriks360@gmail.com>
2023-03-16 22:11:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point of program
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
