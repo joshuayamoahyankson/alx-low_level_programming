02a0f3ea270fbdea5683ad22e05f60c10c9aa42f
joshuayamoahyankson <ziriks360@gmail.com>
2023-03-24 00:13:08 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - the function prototype that multipliers two integers
+ * @a: the functions first parameter to be multiplied
+ * @b: the functions second parameter to be multiplied
+ * Description: Write a function that multiplies two integers.
+ * Prototype: int mul(int a, int b);
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
