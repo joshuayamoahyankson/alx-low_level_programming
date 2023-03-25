d247b58ac6b3b04437482217228d6b5299efcd20
joshuayamoahyankson <ziriks360@gmail.com>
2023-03-24 21:49:20 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void positive_or_negative(int i)
+{
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+}
+
