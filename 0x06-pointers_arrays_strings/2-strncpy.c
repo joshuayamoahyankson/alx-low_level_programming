e78d55409acb4da2ef75612b1adbdfdefa18b9b6
joshuayamoahyankson <ziriks360@gmail.com>
2023-03-31 04:09:20 +0000

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncpy - the function parameter to copy a string
+ * @dest: the first function parameter to copy to
+ * @src: second function parameter to copy
+ * @n: the third function parameter tell step
+ * Description: Write a function that copies a string
+ * Prototype: char *_strncpy(char *dest, char *src, int n);
+ * Your function should work exactly like strncpy
+ * Return: Always (0)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+
+	return (dest);
+}
