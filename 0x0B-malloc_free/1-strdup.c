100cbd5de8c05a2f886887d7bdc78da617273d81
joshuayamoahyankson <ziriks360@gmail.com>
2023-04-14 00:09:46 +0000

    A function that contains a copy of the string given as parameter,1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,24 +12,34 @@
  * Prototype: char *_strdup(char *str);
  * The _strdup() function returns a pointer to a new string
  * which is a duplicate of the string str
- * Memory for the new string is obtained with malloc, and can be freed with free
+ * Memory for the new string is obtained with malloc,
+ * and can be freed with free
  * Returns NULL if str = NULL
- * On success, the _strdup function returns a pointer to the duplicated string
+ * On success, the _strdup function returns a pointer
+ * to the duplicated string
  * It returns NULL if insufficient memory was available
  * Return: Always 0.
  */
 
 char *_strdup(char *str)
 {
-	char *new_string;
+	char *pointer;
+	int i;
 
-	new_string = strdup(str);
-	new_string = malloc(sizeof(str));
-	
-	if (str == NULL)
+	for (i = 0; i < str[i]; ++i)
 	{
-		return NULL;
+		if (str[i] != '\0')
+		{
+			pointer = malloc(i * sizeof(char) + 1);
+		}
 	}
-	_putchar(new_string);
-	return (new_string);
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		pointer[i] = str[i];
+	}
+	return (pointer);
 }
