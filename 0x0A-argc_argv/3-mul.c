0d9916a4780c598e2a456c851f22139030d57dc8
joshuayamoahyankson <ziriks360@gmail.com>
2023-06-10 03:50:24 +0000

    A program that multiplies two numbers, 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * main - the function definition that multiplies two number
@@ -8,14 +9,30 @@
  * Description: Write a program that multiplies two numbers
  * Your program should print the result of the multiplication,
  * followed by a new line
- * You can assume that the two numbers and result of 
+ * You can assume that the two numbers and result of
  * the multiplication can be stored in an integer
- * If the program does not receive two arguments, your program should print Error
+ * If the program does not receive two arguments,
+ * your program should print Error
  * followed by a new line, and return 1
  * Return: Alway 0
  */
 
 int main(int argc, char **argv)
 {
+	int number_1;
+	int number_2;
+	int product;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	number_1 = atoi(argv[1]);
+	number_2 = atoi(argv[2]);
+	product = (number_1 * number_2);
+
+	printf("%d\n", product);
+	return (product);
 }
